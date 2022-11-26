f94c392ab41458713004cbdff68c461c6fb225d1
Kellbber <kellbberbarcarolo@gmail.com>
2022-11-26 19:55:07 -0300

    feat(__app): implemented next/image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { globalStyles } from "../styles/global";
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from "../styles/pages/app";
 
+
+import Image from 'next/image'
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
     <Header>
-      <img src={logoImg.src} alt="logotipo" />
+      <Image src={logoImg} height={50} alt="logotipo do site"/>
     </Header>
       <Component {...pageProps} />
     </Container>
