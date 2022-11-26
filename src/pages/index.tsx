180d59e7752aacdfa45d756249cb04fb974c9509
Kellbber <kellbberbarcarolo@gmail.com>
2022-11-26 19:54:39 -0300

    feat(index): implemented mocked style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,50 @@
-import {styled} from '../styles'
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button',{
-  backgroundColor: '$nft',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span:{
-    fontWeight: "bold"
-  },
-  '&:hover':{
-    filter: 'brightness(0.8)'
-  }
-})
+import Image from "next/image";
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+import camisa1 from "../assets/images/image1.png";
+import camisa2 from "../assets/images/image2.png";
+import camisa3 from "../assets/images/image3.png";
+import camisa4 from "../assets/images/image4.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides:{
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
-   <Button>
-    <span>Teste</span>
-    Enviar</Button>
-  )
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camisa1} width={420} height={380} alt="imagem nft" />
+        <footer>
+          <strong>NFT FIRST</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={420} height={380} alt="imagem nft" />
+        <footer>
+          <strong>NFT FIRST</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={420} height={380} alt="imagem nft" />
+        <footer>
+          <strong>NFT FIRST</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa4} width={420} height={380} alt="imagem nft" />
+        <footer>
+          <strong>NFT FIRST</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
